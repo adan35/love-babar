4e22455af3d1e98abd028ee3f1e2740fb80c8dc3
adan35 <adanshahzad35@gmail.com>
2022-01-17 00:00:13 +0500

    adding median.py

--- array/trapping-water.py
+++ array/trapping-water.py
@@ -1,12 +1,5 @@
 def trappingWater(a):
-    
-    water = 0
-    mini = min(a[0], a[len(a)-1])
-    for i in range(1, len(a)-1):
-        if mini - a[i] >= 0:
-            water += mini - a[i]
-    return water
-
+    pass
 
 a = [8, 8, 2, 4, 5, 5, 1]
 print(trappingWater(a))
