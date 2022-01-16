a9e2b9e10cdff39c210a9bd97bc6a6fcc80a3b42
adan35 <adanshahzad35@gmail.com>
2022-01-17 01:55:29 +0500

    PROBLEM: median of the given array

---
+++ array/median.py
@@ -0,0 +1,15 @@
+from operator import le
+
+
+def median(a):
+    
+    sort = sorted(a)
+    
+    if len(sort) % 2 == 0:
+        return sort[(len(sort) // 2) - 1]
+    else:
+        return sort[len(sort) // 2]
+    
+a = [90, 100, 78, 89, 67]
+print(median(a))
+
